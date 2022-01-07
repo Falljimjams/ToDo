858cbd871704430e63f14b31b492129032cd5d18
Liza Solovec <LizaSolovec@yandex.ru>
2022-01-07 14:33:00 +0300

    Добавлены файлы с лекции

---
+++ index.js
@@ -0,0 +1,42 @@
+const input = document.getElementById("input");
+const output = document.getElementById("output");
+
+output.innerText = "empty";
+output.onclick = onRemove;
+
+const KEYBOARD_KEY__ENTER = "Enter";
+
+const listofTodo = [];
+
+function onRemove(event) {
+    console.timeLog("> onRemove -> event", event.target.dataset.index);
+    const index = event.target.dataset.index;
+    listofTodo.splice(index, 1);
+    document.getElementById(`todo_${index}`);
+}
+input.onkeyup = function(event) {
+    console.log(">onkeyup -> event", event);
+    // console.log("> onkeyup - >input", input.value);
+    if (event.code === KEYBOARD_KEY__ENTER) {
+        const text = input.value;
+
+        if (text.length > 0) {
+            listofTodo.push(text);
+            console.log(`>text = ${listofTodo}`);
+            // output.innerText = listofTodo.join("\n");
+            output.innerHTML = listofTodo
+                .map((value, index) => {
+                    console.log("> listOfTodo -> map", index, value);
+                    return `
+<div id='todo_${index}'>
+<button data-id = '${index}'>x</button>
+<span>${index + 1}. $ {value}</span>
+</div>
+`;
+                })
+                .toString();
+        }
+
+        input.value = "";
+    }
+};
